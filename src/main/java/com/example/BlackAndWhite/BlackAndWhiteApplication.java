197d3513173cb612c3af0d9ade8f2c16adc646c5
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-06-03 05:00:26 -0300

    Setup inicial

---
+++ src/main/java/com/example/BlackAndWhite/BlackAndWhiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlackAndWhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackAndWhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackAndWhiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BlackAndWhite/BlackAndWhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BlackAndWhite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackAndWhiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
