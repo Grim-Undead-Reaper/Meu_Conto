42a2ae7189288c8ccc8a5486df3fa4bd2e351c74
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-06-05 07:01:13 -0300

    Só pra remover o application.properties do histórico msm

--- src/main/java/com/example/BlackAndWhite/Controllers/HomeController.java
+++ src/main/java/com/example/BlackAndWhite/Controllers/HomeController.java
@@ -1,17 +1,32 @@
 package com.example.BlackAndWhite.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 
 @Controller
-@RequestMapping("/homepage")
+@RequestMapping("/BlackAndWhite")
 public class HomeController {
 
     @GetMapping
-    public String getMethodName() {
+    public String Homepage() {
         return "index";
+    }
+
+    @GetMapping("/create")
+    public String Create() {
+        return "create";
+    }
+    
+    @GetMapping("/delete")
+    public String Delete() {
+        return "delete";
+    }
+    
+    @PostMapping("/update")
+    public String Update() {
+        return "update";
     }
     
 
